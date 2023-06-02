532f9c33086983b1e4aff2d13f6ed983f3075de6
GalinggihSeptiar <galinggih2902@gmail.com>
2023-06-02 15:44:34 +0700

    portfoliogalih

---
+++ README.md
@@ -0,0 +1,2 @@
+portfoliogalih
+portfoliogalih
---
+++ js/script.js
@@ -0,0 +1,20 @@
+const textElement = ['Freelancer', 'Web Developer', 'Graphic Designer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt ='';
+let words = '';
+
+(function ngetik() {
+    if(count == textElement.length){
+        count = 0;
+    }
+    currentTxt = textElement[count];
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length == currentTxt.length){
+        count++;
+        txtIndex = 0;
+    }
+    setTimeout(ngetik, 500);
+})();
